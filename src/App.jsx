a01bbb8a3725eaa3479ccf5c6f6d390e97fb63f1
pantho0 <panthomashreky6@gmail.com>
2024-07-17 17:33:45 +0600

    sitewise reload alert added according to cart length

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
+import { useEffect } from "react";
 import MainLayout from "./layouts/MainLayout";
+import { useSelector } from "react-redux";
+import Swal from "sweetalert2";
 
 function App() {
+  const cart = useSelector((state) => state.carts.carts);
+  useEffect(() => {
+    if (cart.length !== 0) {
+      const message = (event) => {
+        event.preventDefault();
+        event.returnValue = "Cart data will be reset";
+      };
+      window.addEventListener("beforeunload", message);
+      return () => window.removeEventListener("beforeunload", message);
+    }
+  }, [cart.length]);
+
   return (
     <>
       <MainLayout />
