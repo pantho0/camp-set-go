ee6da4fd827549d495c656a34cb6272f102e2998
pantho0 <panthomashreky6@gmail.com>
2024-07-17 12:47:54 +0600

    product add form added in modal and image upload and viewing functionalites have been added

--- src/components/modals/AddProductModal.jsx
+++ src/components/modals/AddProductModal.jsx
@@ -1,9 +1,64 @@
 import { Button, Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
+import { useState } from "react";
+import { uploadImage } from "../api/api";
+import toast from "react-hot-toast";
 
 export default function AddProductModal({ isOpen, setIsOpen }) {
+  const [allImages, setImages] = useState([]);
+  const [selectedImage, setSelectedImage] = useState("");
+
   function close() {
     setIsOpen(false);
   }
+
+  const handleSelectImg = (e) => {
+    setSelectedImage(e.target.files[0]);
+  };
+
+  //image upload api call
+  const uploadProductImage = async (image) => {
+    try {
+      const url = await uploadImage(image);
+      if (url) {
+        toast.success("Product Image Uploaded");
+      }
+      setImages([...allImages, url]);
+      setSelectedImage("");
+    } catch (err) {
+      toast.error(
+        "Image upload failed or you are trying to upload in the same field"
+      );
+    }
+  };
+
+  const handleImagesUpload = (e) => {
+    e.preventDefault();
+    const image = selectedImage;
+    uploadProductImage(image);
+  };
+
+  const handleAddProduct = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const category = form.category.value;
+    const price = form.price.value;
+    const stockQuantity = form.quantity.value;
+    const description = form.description.value;
+    const images = allImages;
+
+    const product = {
+      name,
+      category,
+      price,
+      stockQuantity,
+      description,
+      images,
+      ratings: 5.0,
+    };
+
+    console.log(product);
+  };
 
   return (
     <>
@@ -17,7 +72,7 @@ export default function AddProductModal({ isOpen, setIsOpen }) {
           <div className="flex min-h-full items-center justify-center p-4">
             <DialogPanel
               transition
-              className="w-full max-w-md rounded-xl bg-white/5 p-6 backdrop-blur-2xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0"
+              className="w-full max-w-md rounded-xl bg-slate-200 p-6 backdrop-blur-2xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0"
             >
               <DialogTitle
                 as="h3"
@@ -25,16 +80,130 @@ export default function AddProductModal({ isOpen, setIsOpen }) {
               >
                 Add New Product
               </DialogTitle>
-              <p className="mt-2 text-sm/6 text-black">
-                Your payment has been successfully submitted. We’ve sent you an
-                email with all of the details of your order.
-              </p>
+              <div className="px-10 mt-10">
+                <form onSubmit={handleAddProduct}>
+                  <div className="flex flex-col gap-2">
+                    <label className="form-control w-full">
+                      <div className="label">
+                        <span className="label-text">Product Name</span>
+                      </div>
+                      <input
+                        type="text"
+                        name="name"
+                        placeholder="Type here"
+                        className="input input-bordered w-full bg-white"
+                      />
+                    </label>
+                    <label className="form-control w-full ">
+                      <div className="label">
+                        <span className="label-text">Category</span>
+                      </div>
+                      <select
+                        name="category"
+                        className="select select-bordered uppercase bg-white"
+                      >
+                        <option selected disabled></option>
+                      </select>
+                    </label>
+                  </div>
+                  <div className="flex flex-col gap-2">
+                    <label className="form-control w-full">
+                      <div className="label">
+                        <span className="label-text">Price</span>
+                      </div>
+                      <input
+                        type="number"
+                        name="price"
+                        placeholder="Type here"
+                        className="input input-bordered w-full bg-white"
+                      />
+                    </label>
+                  </div>
+                  <div className="flex flex-col gap-2">
+                    <label className="form-control w-full">
+                      <div className="label">
+                        <span className="label-text">Stock Quantity</span>
+                      </div>
+                      <input
+                        type="number"
+                        name="quantity"
+                        placeholder="Type here"
+                        className="input input-bordered w-full bg-white"
+                      />
+                    </label>
+                  </div>
+
+                  <div className="border-2 border-dashed border-secondary p-4 my-2">
+                    {allImages.length === 0 ? (
+                      <p className="text-black">No image selected yet</p>
+                    ) : (
+                      <div className="flex flex-wrap gap-2">
+                        {allImages.map((image, idx) => (
+                          <div key={idx} className="flex gap-2">
+                            <img
+                              className="w-[50px] h-[50px] object-cover"
+                              src={image}
+                              alt=""
+                            />
+                          </div>
+                        ))}
+                      </div>
+                    )}
+                  </div>
+
+                  <div className="flex flex-row gap-2 items-center">
+                    <label className="form-control w-[90%]">
+                      <div className="label">
+                        <span className="label-text">Pictures</span>
+                      </div>
+                      <input
+                        name="picture"
+                        onChange={handleSelectImg}
+                        type="file"
+                        className="file-input file-input-bordered file-input-md w-full"
+                        multiple
+                      />
+                    </label>
+
+                    <div>
+                      <div className="label">
+                        <span className="label-text">max:200kb</span>
+                      </div>
+                      <button
+                        onClick={handleImagesUpload}
+                        className="btn btn-primary bg-green-900 border-none hover:bg-accent hover:text-black"
+                      >
+                        Add Image
+                      </button>
+                    </div>
+                  </div>
+
+                  <div className="flex flex-col md:flex-row gap-2">
+                    <label className="form-control w-full">
+                      <div className="label">
+                        <span className="label-text">Product Description</span>
+                      </div>
+                      <textarea
+                        name="description"
+                        className="textarea textarea-bordered"
+                        placeholder="Bio"
+                      ></textarea>
+                    </label>
+                  </div>
+                  <div className="flex justify-center mt-6 mb-10">
+                    <button className="btn btn-primary bg-green-900 border-none hover:bg-accent hover:text-black hover:btn-accent cursor-pointer">
+                      <input type="submit" value="Update Product" />
+                    </button>
+                  </div>
+                </form>
+              </div>
+
               <div className="mt-4">
                 <Button
                   className="inline-flex items-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[focus]:outline-1 data-[focus]:outline-white data-[open]:bg-gray-700"
                   onClick={close}
                 >
-                  Got it, thanks!
+                  Close
                 </Button>
               </div>
             </DialogPanel>
